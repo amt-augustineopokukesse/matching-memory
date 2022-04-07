e693d4ffccd9ef9fe0e93dbff712d2cade88872f
Augustine Opoku-Kesse <augustine.opoku-kesse@amalitech.org>
2022-04-07 20:33:28 +0100

    Add project files and code

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './app/App.js';
 import reportWebVitals from './reportWebVitals';
 
+// Import the store here.
+import { store } from './app/store.js';
+import { Provider } from 'react-redux';
+
 ReactDOM.render(
-  <React.StrictMode>
+  // Implement Provider component with store below
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
